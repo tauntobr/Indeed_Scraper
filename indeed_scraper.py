5e790dc1104c3fe76ec901940f03060d43364744
tauntobr <tauntobr@users.noreply.github.com>
2015-08-23 21:19:55 -0400

    Update indeed_scraper.py

--- indeed_scraper.py
+++ indeed_scraper.py
@@ -1,3 +1,9 @@
+#Indeed_Scraper v1.0 by tauntobr (Benjamin Taunton). 
+#Created August 23 2015 using Python 2.7.10, BeautifulSoup 4 and lxml 3.4.4
+#This program asks the user to input a job title or keyword and a geographic region (ie city, province/state, country)
+#The program outputs a single .txt file containing the urls returned by the search query on Indeed.com. 
+#Currently returns only the first page of results.
+
 from bs4 import BeautifulSoup
 import urllib2
 
